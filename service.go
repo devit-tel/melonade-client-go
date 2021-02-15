051c86b0eaa959240a6ce2a76eecc79cf281b7f8
NV4RE <tosbodesz@gmail.com>
2021-02-15 18:22:22 +0700

    Add service method

--- service.go
+++ service.go
@@ -9,4 +9,8 @@ import (
 //go:generate mockery -name=Service
 type Service interface {
 	StartWorkflow(ctx context.Context, workflowName, revision, transactionId string, payload interface{}) (*StartWorkflowResponse, goerror.Error)
+	GetWorkflowDefinitions() ([]*WorkflowDefinition, goerror.Error)
+	GetTaskDefinitions() ([]*TaskDefinition, goerror.Error)
+	SetTaskDefinition(t TaskDefinition) goerror.Error
+	SetWorkflowDefinition(t WorkflowDefinition) goerror.Error
 }
